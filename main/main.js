9865a8feb14607cc5385b971ee24c5d3b9e6b2a4
ValenRoss <rodrossi25@gmail.com>
2025-01-23 21:49:56 -0300

    prefinal

--- main/main.js
+++ main/main.js
@@ -1,23 +1,36 @@
 const Api = "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15"
+const carritoDom = document.getElementById("carrito")
+const carrito = []
 
 const Productos = document.getElementById("productos")
 
-function pintadoraDeCards({internalName, salePrice, dealRating, thumb}){
+function creadoraDeCards({internalName, salePrice, dealRating, thumb}){
     const contenedor = document.createElement("div")
+
+    contenedor.classList.add("contenedor")
 
     const titulo = document.createElement("h2")
     const precio = document.createElement("p")
     const Puntuacion = document.createElement("p")
+    const boton = document.createElement("button")
     const img = document.createElement("img")
 
     titulo.innerText = internalName
-    precio.innerText = "$" + salePrice
+    precio.innerText = "U$" + salePrice
     Puntuacion.innerText = dealRating
+    boton.innerText = "Comprar"
     img.src = thumb
 
-    contenedor.append(titulo, precio, Puntuacion, img)
-
+    contenedor.append(titulo, precio, Puntuacion, img, boton)
     Productos.appendChild(contenedor)
+    boton.addEventListener("click", () =>{
+        carrito.push({
+            internalName,
+            salePrice,
+            cantidad: 1
+        })
+    })
+
 }
 
 document.addEventListener("DOMContentLoaded", async ()=>{
@@ -26,7 +39,7 @@ document.addEventListener("DOMContentLoaded", async ()=>{
     console.log(data)
 
     data.forEach(element => {
-        pintadoraDeCards(element)
+        creadoraDeCards(element)
         
     });
 
